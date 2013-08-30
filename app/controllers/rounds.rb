3d4106545155f5a03055d9b451e0c607ca1625b7
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-30 15:22:13 -0500

    developing HTML layout for round.erb

---
+++ app/controllers/rounds.rb
@@ -0,0 +1,13 @@
+#GET===========================================================================================
+
+get '/round/:deck_id' do
+  #@round = Round.create(:deck_id => params[:deck_id], :user_id => session[:user_id])
+  @round = Round.create(:deck_id => 1, :user_id => 4)
+  erb :round
+end
+
+#POST==========================================================================================
+
+post '/round/:round_id' do
+  
+end
