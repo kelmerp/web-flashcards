6da6956d1348978a8bb3284166da1a0baf2fad87
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-30 16:02:34 -0500

    added some css to out HTML layout

--- app/controllers/rounds.rb
+++ app/controllers/rounds.rb
@@ -3,6 +3,8 @@
 get '/round/:deck_id' do
   #@round = Round.create(:deck_id => params[:deck_id], :user_id => session[:user_id])
   @round = Round.create(:deck_id => 1, :user_id => 4)
+  @deck = @round.deck 
+  @card = @deck.cards.where
   erb :round
 end
 
