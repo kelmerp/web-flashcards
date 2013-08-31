4f6760cc2a868c33f316e23c054b002af5211e0f
nlprater <prater.nick@gmail.com>
2013-08-31 14:07:39 -0500

    Debugged User Auth

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -15,17 +15,20 @@ end
 ########## POST ROUTES ##########
 post '/login' do
   @user = User.find_by_email(params[:user][:email])
+  p @user
 
   if @user && @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
+    redirect to "/user/#{@user.id}"
+
   else
+    p "I'm else"
     @login_errors = "Princess Ruby is Sad now.  You have entered\
     and invalid email address or password"
     erb :index
   end
 
-  redirect to "/user/#{@user.id}"
-end
+  end
 
 post '/create' do
   user = User.create(params[:user])
