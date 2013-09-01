98bb7a0610e7abe6a5ce1758a98a734247a449a3
nlprater <prater.nick@gmail.com>
2013-09-01 07:06:34 -0500

    added multiple responses and changed 'FAIL' to 'F'

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -2,6 +2,20 @@ class Guess < ActiveRecord::Base
   belongs_to :card
   belongs_to :round
 
+  def self.get_correct_response
+    correct_responses = ["Wow, that's beautiful!","Super duper job!","You're so smart!","Yeah, way to go!",
+      "You're such a gem!","You are a beautiful princess!","Oh joy!","Congratulations Princess, you got it!"
+      "Oh happy day!"]
+    correct_responses.sample  
+  end
+
+  def self.get_incorrect_response
+    incorrect_responses = ["Boo hoo, you suck!","That answer wasn't very pretty.","That makes me very sad.",
+      "This is depressing, I need to go shopping.","This is quite painful.","Oh dear.",
+      "There were no rainbows in that guess."]
+    incorrect_responses.sample
+  end
+
   def self.correct_first_try(round_id)
     correct_first_try = []
     round_guesses = Guess.where(:round_id => round_id)
